73a78e60bceb7298e8e1ab416336e853cac46643
tigricina <karina6891@mail.ru>
2026-07-30 20:12:41 +0300

    some fix for adaptiva and add some js for header menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector('.mobile-menu');
 
+const openMenu = (event) => {
+    //функция открывания меню
+    menu.classList.add("is-open");  //вешает класс is-open
+    document.body.style.overflow = "hidden";  //запрещает прокрутку сайта под меню
+}
+const closeMenu = (event) => {
+    //функция закрывания меню
+    menu.classList.remove("is-open");  //убирает класс is-open
+    document.body.style.overflow = "";  //возвращает прокрутку сайта под меню
+}
 window.addEventListener("scroll", () => {
     if (this.scrollY > 1) {
         navbar.classList.add("navbar-light");
@@ -14,6 +24,12 @@ window.addEventListener("scroll", () => {
 })
 mMenuToggle.addEventListener("click", (event) => {
     event.preventDefault();
-    menu.classList.toggle('is-open');
+    //menu.classList.toggle("is-open");
+    //openMenu();
+    if (menu.classList.contains('is-open')) { //если меню открыто...
+        closeMenu(); //закрыть меню
+    } else { //иначе...
+        openMenu(); //открыть меню
+    }
 
-})
+});
