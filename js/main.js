cd59ab10a216ac1cc8b20dfe5f00037e733a8817
tigricina <karina6891@mail.ru>
2026-07-31 23:26:11 +0300

    add slider, fix some css

--- js/main.js
+++ js/main.js
@@ -36,3 +36,37 @@ mMenuToggle.addEventListener("click", (event) => {
     //openMenu();
     menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: true,
+
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+
+        576: {
+            slidesPerView: 2,
+
+
+        },
+        // when window width is >= 480px
+        760: {
+            slidesPerView: 3
+
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 4
+
+        },
+        1200: {
+            slidesPerView: 5
+
+        }
+    }
+
+});
