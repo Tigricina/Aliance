55066fa8ad5c4e89ba2e210ad778aa8c24a9b393
tigricina <karina6891@mail.ru>
2026-08-21 20:42:05 +0300

    add telegram send form

--- handler.php
+++ handler.php
@@ -1,5 +1,31 @@
 <?php
-$user_name = $_POST["username"];
-$user_phone = $_POST['userphone'];
-echo "Привет, " . $user_name . "<br>";
-echo "Ваш телефон: <b>" . $user_phone . "</b>";
+$user_name = htmlspecialchars(trim($_POST["username"] ?? ''));
+$user_phone = htmlspecialchars(trim($_POST["userphone"] ?? ''));
+
+if (empty($user_name) || empty($user_phone)) {
+  echo "Error: Все поля обязательны для заполнения.";
+  exit;
+}
+
+$token = "bla";
+$chat_id = "bla";
+
+$text = "Новая заявка с сайта!\n";
+$text .= "Клиент: " . $user_name . "\n";
+$text .= "Телефон: " . $user_phone;
+
+$query = http_build_query([
+  'chat_id' => $chat_id,
+  'text' => $text,
+  'parse_mode' => 'HTML'
+]);
+
+$url = "https://api.telegram.org/bot{$token}/sendMessage?{$query}";
+
+$response = file_get_contents($url);
+
+if($response !== false) {
+  echo "Success";
+} else {
+  echo "Error";
+}
